28709f5438b1178389afd563db053e49015aab01
mathyslechef <myriamgohoure@gmail.com>
2024-12-08 20:06:37 +0100

    mediavision mp3

--- projetmanell.py
+++ projetmanell.py
@@ -19,7 +19,7 @@ import pandas as pd
     #st.dataframe(df)
 
 # Ajout d'un fond sonore
-st.video("https://youtu.be/xMFFubWzDes")
+st.audio("mediavision.mp3")
 
 # Ajout d'un fond d'écran
 page_element="""
